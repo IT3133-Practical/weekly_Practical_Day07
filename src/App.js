d33a10c726d0170346df7dcb5073fdf06955336f
Chamuditha Kavishan Perera <chamudithakavishan2001@gmail.com>
2024-11-23 11:59:38 +0530

    create ap.js

--- src/App.js
+++ src/App.js
@@ -1,73 +1,19 @@
-import React, { useState } from 'react'
-import StudentTable from './components/studentTable/StudentTable'
-import { students } from './StudentsDb'
+import './App.css';
+import { useState } from 'react';
+import Childcom from './Childcom';
 
 function App() {
-  const [fontSize, setFontSize] = useState(20)
-  const [actBtn, setActBtn] = useState('M')
-
-  const fontChng = {
-    fontSize: `${fontSize}px`
-  }
-
-  const actBtnStyl=(btn)=>{
-    return {
-      fontWeight: actBtn === btn ? 'bold' : 'normal', 
-    }
-  }
-
+  const[childName,setChildname]=useState("Default");
+     const receiveName=(name)=>{
+      setChildname(name);
+     }
   return (
-    <>
-      <div className='App'>
-        <h1>Students Information Portal</h1>
-        <hr />
-        <div>
-          <button
-            type='button'
-            onClick={() => {
-              setFontSize(16)
-              setActBtn('S')
-            }}
-            style={actBtnStyl('S')}
-            >
-            S
-          </button>
-
-          <button
-            type='button'
-            onClick={() => {
-              setFontSize(20)
-              setActBtn('M')
-            }}
-            style={actBtnStyl('M')}
-            >
-            M
-          </button>
-
-          <button
-            type='button'
-            onClick={() => {
-              setFontSize(24)
-              setActBtn('L')
-            }}
-            style={actBtnStyl('L')}
-            >
-            L
-          </button>
-        </div>
-        <hr />
-        <table width="100%" style={fontChng}>
-          <tbody>
-            <tr>
-              <td>
-                <StudentTable students={students} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </>
-  )
+    <div className="App">
+      <h2>Hello React I'm Parent</h2>
+      <p>Parent:My child said:{childName}</p>
+      <Childcom setfun={receiveName}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
